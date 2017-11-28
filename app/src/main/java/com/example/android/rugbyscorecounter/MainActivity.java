c26dcbcf32e47632e9ed6611e7996d501f141182
vogel-chr <33537926+vogel-chr@users.noreply.github.com>
2017-11-28 14:06:58 +0100

    update

--- app/src/main/java/com/example/android/rugbyscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/rugbyscorecounter/MainActivity.java
@@ -27,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (savedInstanceState != null) {
+            scoreTeamA = savedInstanceState.getInt("StateScoreA");
+            scoreTeamB = savedInstanceState.getInt("StateScoreB");
+            teamATries = savedInstanceState.getInt("StateTriesA");
+            teamBTries = savedInstanceState.getInt("StateTriesB");
+        }
+        displayForTeamA(scoreTeamA);
+        displayTriesTeamA(teamATries);
+        displayForTeamB(scoreTeamB);
+        displayTriesTeamB(teamBTries);
     }
 
     /**
